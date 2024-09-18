74c1d5d37471d12e17a34aef5f34f982d6385d07
mileidylaura <mileidylaura150@gmail.com>
2024-09-17 23:22:50 -0500

    Cambio 1

--- src/main/java/pe/edu/cibertec/patitas_fronteras/Controller/LoginController.java
+++ src/main/java/pe/edu/cibertec/patitas_fronteras/Controller/LoginController.java
@@ -2,13 +2,48 @@ package pe.edu.cibertec.patitas_fronteras.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import pe.edu.cibertec.patitas_fronteras.viewmodel.LoginModel;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
-    public String inicio(Model model){
 
-        return null;
+
+
+    @GetMapping("/inicio")
+    public String inicio(Model model) {
+        LoginModel loginModel = new LoginModel("00", "", "");
+        model.addAttribute("loginModel", loginModel);
+        return "inicio";
     }
+
+    @PostMapping("/autenticar")
+    public String autenticar(@RequestParam("tipoDocumento") String tipoDocumento,
+                             @RequestParam("numeroDocumento") String numeroDocumento,
+                             @RequestParam("password") String password,
+                             Model model) {
+
+        // Validar campos de entrada
+        if (tipoDocumento == null || tipoDocumento.trim().length() == 0 ||
+                numeroDocumento == null || numeroDocumento.trim().length() == 0 ||
+                password == null || password.trim().length() == 0) {
+
+            //Invocar servicio
+
+            LoginModel loginModel = new LoginModel("01", "Error: Debe completar correctamente sus credenciales", "");
+            model.addAttribute("loginModel", loginModel);
+            return "inicio";
+
+        }
+
+        LoginModel loginModel = new LoginModel("00", "", "Mileidy Gutierrez");
+        model.addAttribute("loginModel", loginModel);
+        return "principal";
+
+    }
+
 }
---
+++ src/main/java/pe/edu/cibertec/patitas_fronteras/viewmodel/LoginModel.java
@@ -0,0 +1,4 @@
+package pe.edu.cibertec.patitas_fronteras.viewmodel;
+
+public record LoginModel(String codigo,String mensaje,String nombreUsuario) {
+}
